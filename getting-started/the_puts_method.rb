c17aefcb54aaafd1eab37f093c5208aab404a16a
leandro brito <leandroabrito89@gmail.com>
2020-07-22 10:35:04 -0300

    puts method

---
+++ getting-started/the_puts_method.rb
@@ -0,0 +1,17 @@
+# string is collection of chars
+
+# puts "hello world"; puts "i'm alive and well"
+
+puts "hello world"
+puts "i'm alive and well"
+
+puts 'i made $65 dollars of a profit of 25%'
+puts 5
+puts 3.12345; puts "3.12345"
+
+puts "4" + "3"
+
+puts
+puts
+
+puts 'bunch of line breaks'

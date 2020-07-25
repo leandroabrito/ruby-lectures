447d1301bcf927fa0d0fb586a1936042914c0507
leandro brito <leandroabrito89@gmail.com>
2020-07-24 21:57:17 -0300

    comments + variables

---
+++ getting-started/comments.rb
@@ -0,0 +1,13 @@
+# this is a comment
+
+# this prints out a string
+puts 'hello world'
+
+# multiline comments could be like this
+# or the way below
+
+=begin
+
+this is another block of comment
+
+=end
---
+++ getting-started/variables.rb
@@ -0,0 +1,23 @@
+# a variable is a reference to an object -> pointer
+# is not an object. is a placeholder for an object
+
+name = "leandro"
+last_name = "brito"
+age = 31
+
+p name
+p last_name
+p age
+
+p name + " " + last_name
+p age + 5
+
+# lower_snake_case
+# case-sensitive
+# right side is always evaluated first
+# dont use ruby keywords
+
+number = 5
+p number
+number = 10
+p number

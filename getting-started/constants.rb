4fb5071de1965e9c5e3f3397f470a8dd0cd5ea0a
leandro brito <leandroabrito89@gmail.com>
2020-07-26 16:21:55 -0300

    parallel assignment + constants

---
+++ getting-started/constants.rb
@@ -0,0 +1,22 @@
+# age = 31
+# p age
+
+# age = 35
+# p age
+
+NAME = "Leandro"
+
+PI = 3.14
+
+p NAME
+
+p PI
+
+
+NAME = "Brito"
+
+PI = 14.3
+
+p NAME
+
+p PI
---
+++ getting-started/parallel_variable_assignment.rb
@@ -0,0 +1,36 @@
+# a = 10
+# b = 20
+# c = 30
+
+# p a
+# p b
+# p c
+
+# p a, b, c
+# puts
+# puts a, b, c
+# puts
+# print a, b, c
+# puts
+
+# parallel variable assignment
+
+a, b, c = 10, 20, 30
+
+p a, b, c
+
+# swapping variable values
+
+a = 1
+b = 2
+
+p a, b
+
+a = 1
+b = 2
+
+puts
+
+a, b = b, a
+
+p a, b

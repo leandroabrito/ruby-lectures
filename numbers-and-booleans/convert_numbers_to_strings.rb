4ac2e545575ae64cddfe010eab9c81ec60ee2aab
leandro brito <leandroabrito89@gmail.com>
2020-07-27 19:39:44 -0300

    intro numbers + convert number to string and vice versa

---
+++ numbers-and-booleans/convert_numbers_to_strings.rb
@@ -0,0 +1,14 @@
+str = "5"
+p str.class # string
+p str.to_i.class # integer
+p str.to_f.class # float
+
+number = 10
+p number.class # integer
+p number.to_s.class # string
+p number.to_f.class # float
+
+pi = 3.14
+p pi.to_i.class # integer
+p pi.to_s.class # string
+
---
+++ numbers-and-booleans/intro_numbers.rb
@@ -0,0 +1,11 @@
+p 5
+p 0
+p 5.20
+p -5
+
+
+p 4.next
+# p 2.12.next
+
+p 80.class
+p 5.20.class
